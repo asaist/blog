a08873f02409969435e9f10625a600fc2726e1ab
kosinevskiy <kosakvova@gmail.com>
2022-04-28 17:01:12 +0400

    123

--- src/main/java/com/itproger/blog/controllers/BlogController.java
+++ src/main/java/com/itproger/blog/controllers/BlogController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 public class BlogController {
 
@@ -36,8 +39,39 @@ public class BlogController {
     }
 
     @GetMapping("/blog/{id}")
-    public String blogDetails(@PathVariable(value = "id") long post id Model model){
-        return "blog-add";
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if(!postRepo.existsById(id)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepo.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepo.existsById(id)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepo.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        Post post = postRepo.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepo.save(post);
+        return "redirect:/blog";
     }
 
 }
